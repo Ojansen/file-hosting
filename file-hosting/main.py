bb0cb3991b9ee6c3b9dc232d33cab8dbb2164ed1
Obe Jansen <obe.jansen@bttr.nl>
2025-07-17 13:15:03 +0200

    feat: Add delete option

--- file-hosting/main.py
+++ file-hosting/main.py
@@ -1,18 +1,33 @@
-from flask import Flask, request, render_template, flash, redirect, url_for
+from io import BytesIO
+
+from bson import ObjectId
+from flask import (
+    Flask,
+    request,
+    render_template,
+    flash,
+    redirect,
+    url_for,
+    send_file,
+    abort,
+    g,
+)
 from pymongo import MongoClient
-import gridfs
+from gridfs import GridFS
 
 app = Flask(__name__)
+app.secret_key = "caf6c365e228accb1fc7bf1977b3a43b4620864b1c874c693f378b45aa8c955f"
 
 client = MongoClient("mongodb://db:27017/?directConnection=true")
 db = client.test_database
-fs = gridfs.GridFS(db)
+fs = GridFS(db)
 
 
 @app.route("/")
-def hello_world():
-    files = fs.list()
-    return render_template("index.html", file=files)
+def index():
+    file_list = fs.list()
+    files = [fs.find_one({"filename": file}) for file in file_list]
+    return render_template("index.html", files=files)
 
 
 @app.route("/upload", methods=["GET", "POST"])
@@ -23,14 +38,26 @@ def upload_file():
             return redirect(request.url)
         file = request.files["file"]
         binary_file = file.stream.read()
-        fs.put(binary_file, filename=file.filename)
-        return redirect(url_for("download_file", file_name=file.filename))
+        fs.put(binary_file, filename=file.filename, content_type=file.content_type)
+        return redirect(url_for("index"))
     else:
         return render_template("upload.html")
 
 
 @app.route("/download/<file_name>")
 def download_file(file_name):
-    file = fs.find_one({"filename": file_name}).read()
-    return render_template("download.html", file=file)
-    # return render_template("download.html", file="nooo")
+    file = fs.find_one({"filename": file_name})
+    return send_file(
+        BytesIO(file.read()),
+        as_attachment=True,
+        mimetype=file.content_type,
+        download_name=file_name,
+    )
+
+
+@app.route("/delete/<file_id>", methods=["POST"])
+def delete_file(file_id):
+    fs.delete(ObjectId(file_id))
+    if fs.exists(ObjectId(file_id)):
+        flash(f"File deleted {file_id}")
+    return redirect(url_for("index"))
